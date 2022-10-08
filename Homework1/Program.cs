f97d22808e39bf28df9af1523a7626aa15f38580
reaper <reaper>
2022-10-08 20:02:56 +0300

    Homework1

---
+++ Homework1/Program.cs
@@ -0,0 +1,28 @@
+﻿int num, rem, sum = 0, temp;
+num = PrintAndGetValue("Введите рассматриваемое число");
+temp = num;
+while (Convert.ToBoolean(num))
+{
+    rem = num % 10;
+    num = num / 10;
+    sum = sum * 10 + rem;
+}
+if (temp == sum)
+{
+    Console.WriteLine("Палиндром найден");
+}
+else
+{
+    Console.WriteLine("Неудача попребуйте другое число");
+}
+
+
+
+
+int PrintAndGetValue(string message)
+{
+    Console.WriteLine("Приступим к проверке является ли число палиндромом");
+    Console.WriteLine(message);
+    int input = int.Parse(Console.ReadLine());
+    return input;
+}
