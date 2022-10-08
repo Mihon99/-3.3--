c5e1843d01b4c5ad8c109e818a20217f029171d0
reaper <reaper>
2022-10-08 22:38:31 +0300

    Homework3

---
+++ Homework3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите желаемое число");
+int n = int.Parse(Console.ReadLine());
+int x = 1;
+if (n < -x)
+{
+    while (n < -x){
+        Console.Write(Math.Pow(n,3) + ", ");
+        n++;}
+    Console.Write(Math.Pow(-x,3) + ".");
+}
+else
+{
+    while (x < n){
+    Console.Write(Math.Pow(x,3) + ", ");
+    x++;}
+Console.Write(Math.Pow(n,3) + ".");
+}
